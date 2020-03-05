70817bf8cd8a19c39d0d256251bf586bb4c8534b
gloomysergei <almazopal@gmail.com>
2020-03-05 09:32:48 +0300

    step_3

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const greeting = () => {
+  const actual = readlineSync.question('May I have your name ');
+  console.log(`Hello, ${actual}!`);
+};
+export default greeting;
