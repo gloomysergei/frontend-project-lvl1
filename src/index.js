18fae11cbc94f01245974c42abed188c56ea2ccf
gloomysergei <almazopal@gmail.com>
2020-03-10 05:49:03 +0300

    step-4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync';
+import {readlineSync} from 'readline-sync';
 const greeting = () => {
   const actual = readlineSync.question('May I have your name ');
   console.log(`Hello, ${actual}!`);
