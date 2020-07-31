344d85e87aa9f831b187dc2f476d13d2b65db6c0
gloomysergei <almazopal@gmail.com>
2020-07-31 20:24:04 +0300

    creation flow final v.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 /* eslint-disable no-param-reassign */
 import readlineSync from 'readline-sync';
 
-// export const greeting = () => {
-//   console.log('Welcome to the Brain Games!');
-//   const name = readlineSync.question('May I have your name  ');
-//   console.log(`Hello, ${name}!`);
-//   return name;
-// };
-
 export const getRandomInt = (min, max) => {
   const counter = Math.floor(Math.random() * (max - min)) + min;
   return counter;
